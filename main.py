7d9106481441c1c47936cf91f9bb4d6a436d8a64
aneesh-kabra <75407296+aneesh-kabra@users.noreply.github.com>
2021-10-23 19:24:49 +0530

    edits

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import flask
+
+from flask import Flask, render_template, request
+from flask import g
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def main():
+  username = "Username"
+  return render_template("dashboard.html",username = username+"!!")
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)`
