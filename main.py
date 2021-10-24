5a127843140238de9d7e5499c87ec37be159378a
Nishchal <71500515+Nish251103@users.noreply.github.com>
2021-10-24 17:37:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+import flask
+from sawo import createTemplate, getContext, verifyToken
+from models import Config
+from sawo import getContext
 
-from flask import Flask, render_template, request
-from flask import g
-import sqlite3
+app = flask.Flask(__name__)
 
 
-app = Flask(__name__)
+createTemplate("./templates/partials",flask=True)
+
+
+class Config(models.Models):
+  api_key = models.CharField(max_length=200)
+  identifier = models.CharField(max_length=200)
+  choices = [("email", "Email"), ("phone_number_sms", "Phone")]
+
+def index(request):
+  config = Config.objects.order_by('-api_key')[:1]
+  context = { "sawo" = getContext(config, "login")
+
+  }
+
+
+
+
+  "sawo": {
+    "auth_key": "7a4f6dd5-0b24-48ab-981e-6630350ad051",
+    "identifier": "email | phone_number_sms"
+                  "to": login
+  }
 
 
 
 @app.route("/")
 def main():
-  username = "Username"
-  return render_template("dashboard.html",username = username+"!!")
+  return flask.render_template("index.html")
 
 
 
-
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)`
+# app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(debug=True)
